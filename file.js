7ff3bbe8658707e58f9382b5d24ce6a1d69e81f4
yamborkor bc2024-3 <amborkorostikk@gmail.com>
2024-10-20 21:36:34 +0300

    Створення основного файлу

--- file.js
+++ file.js
@@ -1,12 +1,42 @@
 const { program } = require('commander');
 const fs = require('fs');
 
-program
-  .option('-f, --file <path>', 'JSON file to read', 'data.json')
-  .parse(process.argv);
 
+program
+  .requiredOption('-i, --input <path>', 'шлях до файлу JSON для читання')
+  .option('-o, --output <path>', 'шлях до файлу для запису результату')
+  .option('-d, --display', 'вивести результат у консоль');
+
+
+program.parse(process.argv);
 const options = program.opts();
-const data = JSON.parse(fs.readFileSync(options.file, 'utf8'));
+
+
+if (!options.input) {
+  console.error('Please, specify input file');
+  process.exit(1);
+}
+
+
+if (!fs.existsSync(options.input)) {
+  console.error('Cannot find input file');
+  process.exit(1);
+}
+
+const data = JSON.parse(fs.readFileSync(options.input, 'utf8'));
 
 const maxRate = Math.max(...data.map(item => item.rate));
-console.log(`Максимальний курс: ${maxRate}`);
+
+
+if (options.display) {
+  console.log(`Максимальний курс: ${maxRate}`);
+}
+
+
+if (options.output) {
+  fs.writeFileSync(options.output, `Максимальний курс: ${maxRate}`);
+}
+
+if (options.output && options.display) {
+  console.log(`Максимальний курс записано у файл: ${options.output}`);
+}
